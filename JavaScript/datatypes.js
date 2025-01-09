826290c77a5d9be8c009839853ecd1d9df97e8f9
Joanna Sharon D <joannasharon.d2023cse@sece.ac.in>
2025-01-09 15:20:15 +0530

    Added JS

---
+++ JavaScript/datatypes.js
@@ -0,0 +1,39 @@
+//Primitive Datatypes
+a="SECE"
+console.log(a)
+b=true
+console.log(b)
+console.log(typeof(a))
+
+//Secondary datatypes(Object)
+array=['a',10,true,[20,30],"Sri Eshwar"]
+console.log(array)
+console.log(typeof(array))
+
+//1st way of declaring an Object 
+obj={
+    firstname:"Sri",
+    lastname:"Eshwar",
+}
+console.log(obj)
+console.log(typeof(obj))
+console.log(obj.firstname)
+console.log(obj.lastname)
+ 
+
+//2nd way of declaring an Object
+obj1={}
+console.log(obj1)
+console.log(typeof(obj1))
+obj1["dept"]="CSE"
+obj1["Section"]="A"
+console.log(obj1)
+
+//3rd way of declaring an Object
+obj2=new Object();
+console.log(obj2)
+obj2.count=70
+obj2.abs=1
+totalPresentCount=obj2.count-obj2.abs
+console.log(totalPresentCount)
+console.log(obj2)
