588ad5b7f00aa349359e819dda437a2fc453d8a0
Joanna Sharon D <joannasharon.d2023cse@sece.ac.in>
2025-01-20 16:05:38 +0530

    Updated

--- seceFrontend/src/assets/components/classComponents/Functional Components/contact.jsx
+++ seceFrontend/src/assets/components/classComponents/Functional Components/contact.jsx
@@ -3,12 +3,16 @@ const contact=()=>{
    // var array=[10,20];
     //var [a,b]= array; //destructuring operator
     var [num,setNum]=useState(0);
+    function handleAdd()
+    {
+        return setNum(num+1);
+    }
     return(
         <div>
             <h4>This is a contact component</h4>
             <h4>This is a state example</h4>
             <h4>Number is {num}</h4>
-            <button onClick={()=>setNum(num+1)}>+</button>
+            <button onClick={()=>{handleAdd()}}>+</button>
         </div>
     )
 }
