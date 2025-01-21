39147dcf8b79c3b00f5f4114a87a9a6694e69021
Joanna Sharon D <joannasharon.d2023cse@sece.ac.in>
2025-01-21 14:09:23 +0530

    Event HAndling

--- seceFrontend/src/assets/components/classComponents/Functional Components/About.jsx
+++ seceFrontend/src/assets/components/classComponents/Functional Components/About.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
+//Event Handling
 const About=()=>{
+    var[text,setText]=useState("SECE")
+    function handleText(event){
+        setText(event.target.value)
+    }
     return(
         <div>
             <h4>This is a about component</h4>
+            <h4>TextArea:</h4>
+            <textarea value={text} onChange={handleText}/>
+            <p>The text is {text}</p>
+
         </div>
     )
 }
